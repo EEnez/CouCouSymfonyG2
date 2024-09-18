3b3b6dd6eb4932a1bdc0ac12f42db34c2f415333
EEnez <enezgubeljic@gmail.com>
2024-09-18 16:28:13 +0200

    update

--- assets/app.js
+++ assets/app.js
@@ -2,9 +2,11 @@ import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
+ * This file will be included onto the page via the import_map() Twig function,
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
